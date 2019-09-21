2f65fc49bff1f77c138c653c44b3825a8cfb55a6
李添财 <narci.ltc@toceansoft.com>
2019-09-21 16:50:30 +0800

    update

---
+++ src/views/Transition.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="demo">
+    <button v-on:click="show = !show">
+      Toggle
+    </button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'tran',
+  components: {
+  },
+  //   el: '#demo',
+
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
